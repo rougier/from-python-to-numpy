d2e3c9d83e21b8decb37ec3a35f326fd3deeafeb
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-11-06 18:07:00 +0100

    Added breadth first solution

--- code/maze_numpy.py
+++ code/maze_numpy.py
@@ -7,13 +7,15 @@ Maze solving using the Bellman-Ford algorithm
 """
 
 import numpy as np
+from collections import deque
 import matplotlib.pyplot as plt
 from scipy.ndimage import generic_filter
+
 
 # -------------------------------------------------------------- build_maze ---
 def build_maze(shape=(65,65), complexity=0.75, density = 0.50):
     """
-    Build a maze with given complexity and density
+    Build a maze using given complexity and density
 
     Parameters
     ==========
@@ -75,13 +77,16 @@ def build_maze(shape=(65,65), complexity=0.75, density = 0.50):
 
 
 # ------------------------------------------------------ find_shortest_path ---
-def find_shortest_path(Z, entrance, exit):
+def BellmanFord(Z, start, goal):
 
+    # We reserve Z such that walls have value 0
+    Z = 1 - Z
+    
     # Build gradient array
     G = np.zeros(Z.shape)
 
     # Initialize gradient at the entrance with value 1
-    G[entrance] = 1
+    G[start] = 1
 
     # Discount factor
     gamma = 0.99
@@ -96,57 +101,88 @@ def find_shortest_path(Z, entrance, exit):
 
     # We iterate until value at exit is > 0. This requires the maze
     # to have a solution or it will be stuck in the loop.
-    while G[exit] == 0.0:
-        for i in range(length):
-            G = Z * generic_filter(G, diffuse, footprint= [[0, 1, 0],
-                                                           [1, 1, 1],
-                                                           [0, 1, 0]])
-        
+    while G[goal] == 0.0:
+        G = Z * generic_filter(G, diffuse, footprint= [[0, 1, 0],
+                                                       [1, 1, 1],
+                                                       [0, 1, 0]])
+    
     # Descent gradient to find shortest path from entrance to exit
-    y, x = exit
+    y, x = goal
     P = []
     dirs = [(0,-1), (0,+1), (-1,0), (+1,0)]
-    # S = Z.astype(float)
-    for i in range(5*(n+n)):
-        P.append((x,y))
-        neighbours = [-1,-1,-1,-1]
+    while (x, y) != start:
+        P.append((x, y))
+        neighbours = [-1, -1, -1, -1]
         if x > 0:
-            neighbours[0] = G[y,x-1]
+            neighbours[0] = G[y, x-1]
         if x < G.shape[1]-1:
-            neighbours[1] = G[y,x+1]
+            neighbours[1] = G[y, x+1]
         if y > 0:
-            neighbours[2] = G[y-1,x]
+            neighbours[2] = G[y-1, x]
         if y < G.shape[0]-1:
-            neighbours[3] = G[y+1,x]
+            neighbours[3] = G[y+1, x]
         a = np.argmax(neighbours)
         x, y  = x + dirs[a][1], y + dirs[a][0]
+    P.append((x, y))
     return G, np.array(P)
+
+def build_graph(maze):
+    height, width = maze.shape
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
+    for row, col in graph.keys():
+        if row < height - 1 and not maze[row + 1][col]:
+            graph[(row, col)].append(("S", (row + 1, col)))
+            graph[(row + 1, col)].append(("N", (row, col)))
+        if col < width - 1 and not maze[row][col + 1]:
+            graph[(row, col)].append(("E", (row, col + 1)))
+            graph[(row, col + 1)].append(("W", (row, col)))
+    return graph
+
+def BreadthFirst(maze):
+    start, goal = (1, 1), (len(maze) - 2, len(maze[0]) - 2)
+    queue = deque([([start], start)])
+    visited = set()
+    graph = build_graph(maze)
+    while queue:
+        path, current = queue.popleft()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            p = list(path)
+            p.append(neighbour)
+            queue.append((p, neighbour))
+    return "NO WAY!"
 
 
 # -------------------------------------------------------------------- main ---
 if __name__ == '__main__':
 
-    n = 41
-    entrance, exit = (-1,-2), (0,1)
-    Z = 1 - build_maze((51,51))
-    Z[entrance] = 1
-    Z[exit] = 1
-
-    G, P = find_shortest_path(Z, entrance, exit)
+    n = 51
+    start, goal = (1,1), (n-2,n-2)
+    Z = build_maze((n,n))
+    
+    G, P = BellmanFord(Z, start, goal)
     X, Y = P[:,0], P[:,1]
 
-    # Visualization
+    # P = np.array(BreadthFirst(Z))
+    # X, Y = P[:,1], P[:,0]
+    
+    # Visualization maze, gradient and shortest path
     plt.figure(figsize=(9,9))
     ax = plt.subplot(1, 1, 1, frameon=False)
-    ax.imshow(G, interpolation='nearest', cmap=plt.cm.hot, vmin=0, vmax=1)
-    ax.scatter(X, Y, s=60, lw=1, color='k', marker='o', edgecolors='k', facecolors='w')
+    ax.imshow(Z, interpolation='nearest', cmap=plt.cm.gray_r, vmin=0.0, vmax=1.0)
+    cmap = plt.cm.hot
+    cmap.set_under(color='k', alpha=0.0)
+    ax.imshow(G, interpolation='nearest', cmap=cmap, vmin=0.01, vmax=G[start])
+    ax.scatter(X[1:-1], Y[1:-1], s=60,
+               lw=1, marker='o', edgecolors='k', facecolors='w')
+    ax.scatter(X[[0,-1]], Y[[0,-1]], s=60,
+               lw=3, marker='x', color=['w','k'])
     ax.set_xticks([])
     ax.set_yticks([])
-
-    ax.text(1.0, -1.25, "Exit",
-            transform=ax.transData, fontsize=12, ha = 'center', va = 'center')
-    ax.text(G.shape[0]-2, G.shape[1]+0.25, "Entrance",
-            transform=ax.transData, fontsize=12, ha = 'center', va = 'center')
-
     plt.tight_layout()
     plt.show()
+
