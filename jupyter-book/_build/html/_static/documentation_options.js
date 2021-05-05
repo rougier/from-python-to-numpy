77a864b21c8373ed22fa322c867956e5ef48cf9d
loic <loic.houpert@ose-engineering.fr>
2021-05-05 14:28:05 +0100

    rerun html built

--- jupyter-book/_build/html/_static/documentation_options.js
+++ jupyter-book/_build/html/_static/documentation_options.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'singlehtml',
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
